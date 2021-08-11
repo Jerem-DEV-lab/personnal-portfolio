07d991b55f3f90059e6267e34ec4b2f53c7897a5
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-08-12 01:04:17 +0200

    feat: Review client

---
+++ resources/js/Swiper.js
@@ -0,0 +1,34 @@
+import Swiper from "swiper";
+const hamburger_menu  = document.querySelector(".hamburger-menu");
+const navbar          = document.querySelector("header nav");
+const links           = document.querySelectorAll(".links a");
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
+
+const mySwiper = new Swiper(".swiper-container", {
+  speed        : 1100,
+  slidesPerView: 1,
+  loop         : true,
+  autoplay     : {
+    delay: 5000,
+  },
+  navigation   : {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+});
