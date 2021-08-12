d040aae51202465ac47d227522346d992ffbd215
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-08-12 22:50:07 +0200

    ref / Feat: Fix responsive , formulaire de contact

--- resources/js/Swiper.js
+++ resources/js/Swiper.js
@@ -1,7 +1,7 @@
 import Swiper from "swiper";
 const hamburger_menu  = document.querySelector(".hamburger-menu");
 const navbar          = document.querySelector("header nav");
-const links           = document.querySelectorAll(".links a");
+const links           = document.querySelectorAll(".nav-links a");
 
 function closeMenu() {
   navbar.classList.remove("open");
