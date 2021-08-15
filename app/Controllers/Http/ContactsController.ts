aa676f1ec5587ac3d99cf315f82222d12294f02c
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2021-08-15 16:54:25 +0200

    Affichage du message de succès lors du contact

---
+++ app/Controllers/Http/ContactsController.ts
@@ -0,0 +1,17 @@
+import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import ContactValidator from "App/Validators/ContactValidator";
+import Messages from "App/Models/Messages";
+
+export default class ContactsController {
+  public async contactMessage({request, response, session}: HttpContextContract) {
+    try {
+      const payload = await request.validate(ContactValidator);
+      await Messages.create(payload)
+      session.flash('successContact', "Votre message à bien été envoyer")
+      return response.redirect().toPath('/#contact')
+    } catch (e) {
+      return e
+    }
+  }
+}
+
---
+++ database/migrations/1629036696398_messages.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Messages extends BaseSchema {
+  protected tableName = 'messages'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('last_name').notNullable()
+      table.string('first_name').notNullable()
+      table.string('email').notNullable()
+      table.string('message').notNullable()
+      table.string('phone').notNullable()
+      table.timestamp('created_at', {useTz: true})
+      table.timestamp('updated_at', {useTz: true})
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
