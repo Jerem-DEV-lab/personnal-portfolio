68fddd9201b92574cc310bf1845c4f840ae18441
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2022-02-15 23:30:13 +0100

    fix header

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -5,7 +5,9 @@ export default class HomeController {
   public async index({view}: HttpContextContract) {
     const projects = await Projects.query()
     return view.render('index', {projects})
-
+  }
+  public async viewCv({view}: HttpContextContract) {
+    return view.render('pages/cv')
   }
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'HomeController.index')
-
+Route.get('/cv-guillemet-jeremy', 'HomeController.viewCv')
 Route.post('contact', 'ContactsController.contactMessage')
